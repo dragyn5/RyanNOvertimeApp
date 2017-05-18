fce83d5d1e760e42f27efc48840fc940ded67862
Ryan <nygardryan@yahoo.com>
2017-05-18 16:58:59 -0600

    added styles to status tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  def status_span_generator status
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'label label-danger')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    end
+  end
 end
