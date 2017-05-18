fd9626b3e22763bbd35f4524a28a21546555e2fc
Ryan <nygardryan@yahoo.com>
2017-05-18 15:49:23 -0600

    Implemented new index query, showing user posts while hiding others

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -8,5 +8,12 @@ FactoryGirl.define do
     date Date.yesterday
     rationale "Some more Rationale"
     user
+  end  
+  factory :post_from_other_user, class: "Post" do
+    date Date.yesterday
+    rationale "Some more Rationale"
+    non_authorized_user
   end
+
+
 end
